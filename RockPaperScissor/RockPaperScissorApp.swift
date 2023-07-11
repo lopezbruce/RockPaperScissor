6877a849c51c7408d2eb0c6b2c7cfa9ae3e508d5
Bruce Lopez <lopez.bruce@gmail.com>
2023-07-11 07:02:05 -0400

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Bruce Lopez on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
